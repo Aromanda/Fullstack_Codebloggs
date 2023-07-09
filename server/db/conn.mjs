77de0e3fee5be52f30703f5e856ed504c8e08077
murphylexus <murphy_lexus@hotmail.com>
2023-07-09 18:52:36 -0400

    working starting project

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -11,6 +11,27 @@ try {
   console.error(e);
 }
 
-let db = conn.db("CODEBLOGGS");
+let db = conn.db("full_stack");
+
+// Specify the collection
+const collection = db.collection('session');
+
+// Check if index on 'createdAt' field exists
+const indexInfo = await collection.indexInformation();
+const indexExists = !!indexInfo['createdAt_1'];
+
+// If it exists, drop it
+if (indexExists) {
+  await collection.dropIndex('createdAt_1');
+}
+
+// Create a TTL index on the 'createdAt' field. The 'expireAfterSeconds' value of 86400 means 
+// that the documents will expire 24 hours after the time specified in the 'createdAt' field.
+try {
+  await collection.createIndex( { "createdAt": 1 }, { expireAfterSeconds: 86400 } );
+  console.log("TTL index created successfully");
+} catch(e) {
+  console.error(e);
+}
 
 export default db;
