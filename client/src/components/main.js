b0728d299bb8fb793f84e651438fbddf5c170b07
murphylexus <murphy_lexus@hotmail.com>
2023-07-10 19:38:17 -0400

    navbar

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import Sidebar from './sidebar'; 
 
 const Main = () => {
-  const linkStyle = {
-    fontSize: "20px",
-    fontStyle: "italic"
-  };
-
   return (
     <Container fluid style={{ backgroundColor: "#8D88EA", height: "100vh", padding: 0 }}>
-      <Navbar bg="light" variant="light" style={{ height: "100%", width: "15%", position: "fixed", zIndex: 1 }}>
-        <Nav className="flex-column" style={{ height: "100%" }}>
-          <Nav.Link href="#home" style={linkStyle}>Home</Nav.Link>
-          <Nav.Link href="#bloggs" style={linkStyle}>Bloggs</Nav.Link>
-          <Nav.Link href="#network" style={linkStyle}>Network</Nav.Link>
-        </Nav>
-      </Navbar>
+      <Sidebar /> {/* here we're including the Sidebar */}
       <div style={{ marginLeft: "15%", padding: "20px" }}>
         <h1>Welcome to the Main Page!</h1>
       </div>
