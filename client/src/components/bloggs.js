89084afa4ed5096e2003d3b03003619da777660d
murphylexus <murphy_lexus@hotmail.com>
2023-07-11 15:38:59 -0400

    working post with postman

--- client/src/components/bloggs.js
+++ client/src/components/bloggs.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import Sidebar from './sidebar'; 
 
 const Main = () => {
+  const [posts, setPosts] = useState([]); // Initialize state to hold your posts
+
+  // Define your function to fetch data
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:5050/post");
+      console.log(response.ok)
+      if(response.ok){
+        const data = await response.json();
+        setPosts(data); // Update your state with the fetched data
+      }else{
+        throw new Error('Error while fetching posts');
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  // Use useEffect to call your fetchData function when the component mounts
+  useEffect(() => {
+    fetchData();
+  }, []); 
+
   return (
     <Container fluid style={{ backgroundColor: "#8D88EA", height: "100vh", padding: 0 }}>
       <Sidebar /> {/* here we're including the Sidebar */}
       <div style={{ marginLeft: "15%", padding: "20px" }}>
-        <h1>Welcome to the Bloggs Page!</h1>
+        <h1>Welcome to the Blog Page!</h1>
+        {/* Map over your posts and display them */}
+        {posts.map((post, index) => (
+          <div key={index}>
+            <p>{post.content}</p>
+          </div>
+        ))}
       </div>
     </Container>
   );
--- server/db/schemas/post.schemas.mjs
+++ server/db/schemas/post.schemas.mjs
@@ -2,25 +2,21 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const CommentSchema = new Schema({
-  content: String,
-  user_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  likes: Number,
-  time_stamp: String,
-});
-
 const PostSchema = new Schema({
   content: String,
   user_id: {
     type: Schema.Types.ObjectId,
     ref: 'User',
   },
-  likes: Number,
+  likes: {
+    type: Number,
+    default:0
+  },
   time_stamp: String,
-  comments: [CommentSchema],
+  comments: [{
+    type: Schema.Types.ObjectId, 
+    ref:"Comment"
+  }] 
 });
 
 export default mongoose.model('Post', PostSchema);
