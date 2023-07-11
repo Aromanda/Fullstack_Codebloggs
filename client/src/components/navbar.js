3167f831b343cef67b1e74aebb691f0ccac7e3d3
murphylexus <murphy_lexus@hotmail.com>
2023-07-10 21:17:40 -0400

    fin de journee

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
-import { Nav, Navbar, Dropdown, Image } from "react-bootstrap";
+import { Navbar, Dropdown, Button, Modal, Form, Image } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function MyNavbar() {
   const [agentName, setAgentName] = useState("");
   const location = useLocation();
   const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+  const [newPostContent, setNewPostContent] = useState("");
 
   useEffect(() => {
     async function fetchAgentData() {
@@ -24,11 +26,23 @@ export default function MyNavbar() {
     fetchAgentData();
   }, []);
 
+  const handleModalOpen = () => setShowModal(true);
+  const handleModalClose = () => setShowModal(false);
+
+  const handlePostContentChange = (event) => setNewPostContent(event.target.value);
+
   async function handleLogout() {
     await fetch('http://localhost:5050/logout', { method: 'POST', credentials: 'include' });
     sessionStorage.clear();
     document.cookie = 'connect.sid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
     navigate('/');
+  }
+
+  async function handleSubmitPost() {
+    // Handle the submission of the new post here
+    // After submitting, close the modal and clear the post content
+    handleModalClose();
+    setNewPostContent("");
   }
 
   const isLoginPage = location.pathname === "/login";
@@ -41,24 +55,38 @@ export default function MyNavbar() {
     width: "30%", 
   };
 
-  if (isLoginPage || isCreatePage ) {
+  if (isLoginPage || isCreatePage) {
     logoStyle.width = "50%"; 
   } else if (isMainPage || isBloggsPage || isNetworkPage) {
     logoStyle.width = "40%"; 
   }
 
+  const postButtonStyle = {
+    backgroundColor: "#8D88EA",
+    color: "white",
+    fontSize: "20px",
+    padding: "10px 120px",
+    marginRight: "auto"
+  };
+
+  const modalContainerStyle = {
+    backgroundColor: "#f8f9fa" // Set the desired container color here
+  };
+
+  const submitButtonStyle = {
+    backgroundColor: "black",
+    color: "white"
+  };
+
   return (
     <div>
-      <Navbar expand="lg" bg="light" variant="light">
+      <Navbar expand="lg" bg="light" variant="light" style={{ display: "flex", justifyContent: "space-between" }}>
         <Navbar.Brand as={NavLink} to="/">
           <Image style={logoStyle} src="/images/codebloggs/codebloggs logo2.png" alt="Logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            {/* Other Nav links here */}
-          </Nav>
-        </Navbar.Collapse>
+        <Button style={postButtonStyle} onClick={handleModalOpen}>
+          Post
+        </Button>
         {!(location.pathname === "/" || location.pathname === "/create") && (
           <Dropdown alignRight>
             <Dropdown.Toggle 
@@ -81,6 +109,27 @@ export default function MyNavbar() {
             </Dropdown.Menu>
           </Dropdown>
         )}
+        <Modal show={showModal} onHide={handleModalClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Blog something</Modal.Title>
+          </Modal.Header>
+          <Modal.Body style={modalContainerStyle}>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={newPostContent}
+              onChange={handlePostContentChange}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleModalClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleSubmitPost} style={submitButtonStyle}>
+              Submit Post
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Navbar>
     </div>
   );
