a713d4c579951a9db978d77c7f6cd845016e1eb7
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-12 12:38:52 -0400

    POST sur /post FONCTIONNEL, DEBUT PROJET

--- server/db/routes/post.mjs
+++ server/db/routes/post.mjs
@@ -2,38 +2,27 @@ import express from "express";
 import db from "../conn.mjs";
 import cors from "cors";
 import { ObjectId } from "mongodb";
-import PostSchema from "../schemas/post.schemas.mjs";
-
+import PostSchemas from "../schemas/post.schemas.mjs";
 const router = express.Router();
 router.use(cors());
-
 const collection = db.collection("post");
-
 // Create a new post
 router.post("/", async (req, res) => {
   try {
     // Create a new post object
-    const newPost = new PostSchema(req.body);
-
+    const newPost = new PostSchemas(req.body);
+    const collection = db.collection("post");
     // Insert the new post into the database
     const result = await collection.insertOne(newPost);
-    res.status(201).json({ message: "Post created successfully", post: result.ops[0] });
-
+    const response = {
+      user_id: req.body.user_id,
+      post: result
+    };
+    console.log(response);
+    res.status(201).json({ message: "Post created successfully", response });
   } catch (error) {
     console.error("Error creating the post:", error);
     res.status(500).json({ error: "Failed to create the post" });
   }
 });
-
-// Get all posts
-router.get("/", async (req, res) => {
-  try {
-    const results = await collection.find({}).toArray();
-    res.status(200).json(results);
-  } catch (error) {
-    console.error("Error getting the posts:", error);
-    res.status(500).json({ error: "Failed to get the posts" });
-  }
-});
-
 export default router;
