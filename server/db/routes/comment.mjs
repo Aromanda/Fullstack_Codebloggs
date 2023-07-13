2fb073d239b429a5de9674c45dd096613b0f7ccc
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-13 16:00:36 -0400

    create Get comment

--- server/db/routes/comment.mjs
+++ server/db/routes/comment.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import db from '../conn.mjs'; 
 import CommentSchema from '../schemas/comment.schemas.mjs'; 
+import { ObjectId } from 'mongodb';
 
 const router = express.Router();
 
@@ -23,52 +24,18 @@ router.post('/', async (req, res) => {
   }
 });
 
-// router.get('/:userID', async (req, res) => {
-//     try {
-//       const collection = db.collection('comment');
-//       const { userID } = req.params;
-//         console.log(userID);
-//       // Find all comments made by the specified user
-//       const comments = await collection.find({ user_id: userID }).toArray();
-//       console.log(comments);
-      
-//       // Customize the response to include specific fields from the CommentSchema
-//       const formattedComments = comments.map((comment) => {
-//         const { _id, likes, content, user_id, post_id, time_stamp } = comment;
-//         return {
-//           _id,
-//           likes,
-//           content,
-//           user_id,
-//           post_id,
-//           time_stamp,
-//         };
-//       });
-//       console.log(formattedComments);
-//       // Send the formatted comments as a response
-//       res.status(200).json({ comments: formattedComments });
-//     } catch (error) {
-//       // Send an error response if there's any issue
-//       console.error(error);
-//       res.status(500).json({ message: 'An error occurred while retrieving the comments.' });
-//     }
-//   });
-
-  router.get('/:userID', async (req, res) => {
+  router.get('/:postID', async (req, res) => { 
   try {
-    const { userID } = req.params;
-    
-    // Get the "posts" collection from the "MERNreview" database
-    const collection = client.db("MERNreview").collection("posts");
+    const { postID } = req.params;
+    console.log(postID);
+    const collection = db.collection('comment');
 
-    // Perform the aggregation query
-    const comments = await collection.aggregate([
-      { $match: { user_id: userID } },
-      // Add any additional stages you need for the aggregation pipeline
-    ]).toArray();
+    let query = { post_id: new ObjectId(postID) };
+    let result = await collection.find(query).toArray();
+    console.log(result);
 
     // Send the comments as a response
-    res.status(200).json({ comments });
+    res.status(200).json({ result });
   } catch (error) {
     // Send an error response if there's any issue
     console.error(error);
