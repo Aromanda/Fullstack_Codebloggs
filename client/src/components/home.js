7b4dddf4597b351c5c2cd592e5d01bb4c373a8dd
ET GLap <eglapointe@hotmail.com>
2023-07-13 10:34:51 -0400

    Fix: receive props userId

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Sidebar from './sidebar'; 
 
-const Main = () => {
+const Main = (props) => {
+    const {userId} = props
+    console.log(`HOME MOTHER FUCKING PAGE: `+userId)
   return (
     <Container fluid style={{ backgroundColor: "#8D88EA", height: "100vh", padding: 0 }}>
       <Sidebar /> {/* here we're including the Sidebar */}
