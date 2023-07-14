1d6b02c8adfce62a59c5f985c2918fa596aadd82
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-14 18:17:16 -0400

    update comment.mjs

--- server/db/routes/comment.mjs
+++ server/db/routes/comment.mjs
@@ -1,20 +1,18 @@
 import express from 'express';
-import db from '../conn.mjs'; 
-import CommentSchema from '../schemas/comment.schemas.mjs'; 
+import db from '../conn.mjs';
+import CommentSchema from '../schemas/comment.schemas.mjs';
 import { ObjectId } from 'mongodb';
-
 const router = express.Router();
-
-// Handle POST request to /comment endpoint
 router.post('/', async (req, res) => {
   try {
     const collection = db.collection('comment');
     // Create a new Comment document
-    const newComment = new CommentSchema(req.body);
-
+    const newComment = new CommentSchema({
+      ...req.body,
+      user_ID: req.user_ID
+    });
     // Save the comment to the MongoDB collection
     await collection.insertOne(newComment);
-
     // Send a success response
     res.status(201).json({ message: 'Comment saved successfully!' });
   } catch (error) {
@@ -23,17 +21,14 @@ router.post('/', async (req, res) => {
     res.status(500).json({ message: 'An error occurred while saving the comment.' });
   }
 });
-
-  router.get('/:postID', async (req, res) => { 
+  router.get('/:postID', async (req, res) => {
   try {
     const { postID } = req.params;
     console.log(postID);
     const collection = db.collection('comment');
-
     let query = { post_id: new ObjectId(postID) };
     let result = await collection.find(query).toArray();
     console.log(result);
-
     // Send the comments as a response
     res.status(200).json({ result });
   } catch (error) {
@@ -42,6 +37,4 @@ router.post('/', async (req, res) => {
     res.status(500).json({ message: 'An error occurred while retrieving the comments.' });
   }
 });
-
-  
 export default router;
