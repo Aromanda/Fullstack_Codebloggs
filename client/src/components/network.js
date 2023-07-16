074ed73b25ee492b07c08410f3ed03aee83b92ee
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-16 11:39:13 -0400

    update network page fonctionnel

--- client/src/components/network.js
+++ client/src/components/network.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import Sidebar from "./sidebar";
 const Main = ({ userId }) => {
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState([]);
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const response = await fetch(`http://localhost:5050/user/${userId}`);
+        const response = await fetch("http://localhost:5050/user/");
         const data = await response.json();
         setUserData(data);
       } catch (error) {
@@ -14,7 +14,7 @@ const Main = ({ userId }) => {
       }
     };
     fetchUserData();
-  }, [userId]);
+  }, []);
   const getInitials = (name) => {
     if (!name) {
       return "";
@@ -32,53 +32,57 @@ const Main = ({ userId }) => {
         <Col xs={2}>
           <Sidebar />
         </Col>
-        <Col xs={4}>
+        <Col xs={10}>
           <div style={{ padding: "20px" }}>
             <h1>Welcome to the network Page!</h1>
-            {userData && (
-              <Card>
-                <Card.Body>
-                  <Row>
-                    <Col xs={2} style={{ display: "flex", alignItems: "center" }}>
-                      <div
-                        style={{
-                          backgroundColor: "black",
-                          color: "white",
-                          width: "60px",
-                          height: "60px",
-                          fontSize: "20px",
-                          display: "flex",
-                          alignItems: "center",
-                          justifyContent: "center",
-                          borderRadius: "50%",
-                          marginRight: "20px",
-                        }}
-                      >
-                        {getInitials(`${userData.first_name} ${userData.last_name}`)}
-                      </div>
-                    </Col>
-                    <Col xs={4}>
-                      <h3>{userData.name}</h3>
-                      <Card.Text>
-                        <strong>Email:</strong> {userData.email}
-                        <br />
-                        <strong>First Name:</strong> {userData.first_name}
-                        <br />
-                        <strong>Last Name:</strong> {userData.last_name}
-                        <br />
-                        <strong>Location:</strong> {userData.location}
-                        <br />
-                        <strong>Birthdate:</strong> {userData.birthdate}
-                        <br />
-                        <strong>Occupation:</strong> {userData.occupation}
-                        <br />
-                        <strong>Status:</strong> {userData.status}
-                      </Card.Text>
-                    </Col>
-                  </Row>
-                </Card.Body>
-              </Card>
-            )}
+            <Row>
+              {userData.map((user) => (
+                <Col key={user.id} xs={4} style={{ marginBottom: "20px" }}>
+                  <Card>
+                    <Card.Body>
+                      <Row>
+                        <Col xs={2} style={{ display: "flex", alignItems: "center" }}>
+                          <div
+                            style={{
+                              backgroundColor: "black",
+                              color: "white",
+                              width: "60px",
+                              height: "60px",
+                              fontSize: "20px",
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                              borderRadius: "50%",
+                              marginRight: "20px",
+                            }}
+                          >
+                            {getInitials(`${user.first_name} ${user.last_name}`)}
+                          </div>
+                        </Col>
+                        <Col xs={10}>
+                          <h3>{user.name}</h3>
+                          <Card.Text>
+                            <strong>Email:</strong> {user.email}
+                            <br />
+                            <strong>First Name:</strong> {user.first_name}
+                            <br />
+                            <strong>Last Name:</strong> {user.last_name}
+                            <br />
+                            <strong>Location:</strong> {user.location}
+                            <br />
+                            <strong>Birthdate:</strong> {user.birthdate}
+                            <br />
+                            <strong>Occupation:</strong> {user.occupation}
+                            <br />
+                            <strong>Status:</strong> {user.status}
+                          </Card.Text>
+                        </Col>
+                      </Row>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
           </div>
         </Col>
       </Row>
