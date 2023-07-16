bc1aad0f332dc0b3c369bee18f98e9b618d46933
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-16 11:30:32 -0400

    network update avec les initiales

--- client/src/components/network.js
+++ client/src/components/network.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import Sidebar from "./sidebar";
-
-
 const Main = ({ userId }) => {
   const [userData, setUserData] = useState(null);
-  const [posts, setPosts] = useState([]);
   useEffect(() => {
     const fetchUserData = async () => {
       try {
@@ -18,50 +15,67 @@ const Main = ({ userId }) => {
     };
     fetchUserData();
   }, [userId]);
-
-  // const fetchPosts = async () => {
-  //   const response = await fetch("http://localhost:5050/post/");
-  //   console.log(response);
-  //   if (response.ok) {
-  //     const data = await response.json();
-  //     setPosts(data.result);
-  //     data.result.forEach((post) => {
-  //       fetchUser(post.user_id);
-  //     });
-  //     console.log(data);
-  //   } else {
-  //     console.error("Failed to fetch posts. Please try again.");
-  //   }
-  // };
-
+  const getInitials = (name) => {
+    if (!name) {
+      return "";
+    }
+    const names = name.split(" ");
+    const initials = names
+      .map((n) => n.charAt(0))
+      .join("")
+      .toUpperCase();
+    return initials;
+  };
   return (
     <Container fluid style={{ backgroundColor: "#8D88EA", height: "100vh", padding: 0 }}>
       <Row>
         <Col xs={2}>
           <Sidebar />
         </Col>
-        <Col xs={10}>
+        <Col xs={4}>
           <div style={{ padding: "20px" }}>
             <h1>Welcome to the network Page!</h1>
             {userData && (
               <Card>
                 <Card.Body>
-                  <Card.Title>{userData.name}</Card.Title>
-                  <Card.Text>
-                    <strong>Email:</strong> {userData.email}
-                    <br />
-                    <strong>first_name:</strong> {userData.first_name}
-                    <br />
-                    <strong>last_name:</strong> {userData.last_name}
-                    <br />
-                    <strong>Location:</strong> {userData.location}
-                    <br />
-                    <strong>birthdate:</strong> {userData.birthdate}
-                    <br />
-                    <strong>occupation:</strong> {userData.occupation}
-                    <br />
-                    <strong>status:</strong> {userData.status}
-                  </Card.Text>
+                  <Row>
+                    <Col xs={2} style={{ display: "flex", alignItems: "center" }}>
+                      <div
+                        style={{
+                          backgroundColor: "black",
+                          color: "white",
+                          width: "60px",
+                          height: "60px",
+                          fontSize: "20px",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          borderRadius: "50%",
+                          marginRight: "20px",
+                        }}
+                      >
+                        {getInitials(`${userData.first_name} ${userData.last_name}`)}
+                      </div>
+                    </Col>
+                    <Col xs={4}>
+                      <h3>{userData.name}</h3>
+                      <Card.Text>
+                        <strong>Email:</strong> {userData.email}
+                        <br />
+                        <strong>First Name:</strong> {userData.first_name}
+                        <br />
+                        <strong>Last Name:</strong> {userData.last_name}
+                        <br />
+                        <strong>Location:</strong> {userData.location}
+                        <br />
+                        <strong>Birthdate:</strong> {userData.birthdate}
+                        <br />
+                        <strong>Occupation:</strong> {userData.occupation}
+                        <br />
+                        <strong>Status:</strong> {userData.status}
+                      </Card.Text>
+                    </Col>
+                  </Row>
                 </Card.Body>
               </Card>
             )}
