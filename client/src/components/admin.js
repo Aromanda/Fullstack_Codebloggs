c303b9633378b867134c265f7ea6c47b41264638
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-17 18:26:31 -0400

    admin apres modal pour video

--- client/src/components/admin.js
+++ client/src/components/admin.js
@@ -1,35 +1,31 @@
-import React from "react";
+import React from 'react';
 import { Container, Card, Row, Col } from 'react-bootstrap';
-import Sidebar from './sidebar'; 
-
+import Sidebar from './sidebar';
 const Main = () => {
+  const handleClick = () => {
+    alert('The feature will be available shortly!');
+  };
   return (
-    <Container fluid style={{ backgroundColor: "#8D88EA", height: "100vh", padding: 0 }}>
-      <Sidebar /> {/* Here we're including the Sidebar */}
-      <div style={{ marginLeft: "15%", padding: "20px" }}>
+    <Container fluid style={{ backgroundColor: '#8D88EA', height: '100vh', padding: 0 }}>
+      <Sidebar />
+      <div style={{ marginLeft: '15%', padding: '20px' }}>
         <h1>Welcome to the Admin Page!</h1>
         <Row>
           <Col md={6} style={{ marginBottom: '20px' }}>
-            <Card>
+            <Card onClick={handleClick} style={{ cursor: 'pointer' }}>
               <Card.Body>
                 <Card.Title>User Manager</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card 1 Subtitle</Card.Subtitle>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
+                <Card.Subtitle className="mb-2 text-muted">Admin access only</Card.Subtitle>
+                <Card.Text></Card.Text>
               </Card.Body>
             </Card>
           </Col>
           <Col md={6} style={{ marginBottom: '20px' }}>
-            <Card>
+            <Card onClick={handleClick} style={{ cursor: 'pointer' }}>
               <Card.Body>
                 <Card.Title>Content Manager</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card 2 Subtitle</Card.Subtitle>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
+                <Card.Subtitle className="mb-2 text-muted">Admin access only</Card.Subtitle>
+                <Card.Text></Card.Text>
               </Card.Body>
             </Card>
           </Col>
@@ -38,5 +34,4 @@ const Main = () => {
     </Container>
   );
 }
-
 export default Main;
