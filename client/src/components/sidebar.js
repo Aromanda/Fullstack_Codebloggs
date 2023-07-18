42f7a980b716aed5b1d25edcf5e58c4d6b7a7135
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-18 15:52:44 -0400

    sidebar fonctionnel avce admin user auth level

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
 const linkStyle = {
-  color: "white", // Change text color to white
-  fontSize: "22px", 
-  fontWeight: "bold", 
-  paddingTop: "20px"
+  color: 'white',
+  fontSize: '22px',
+  fontWeight: 'bold',
+  paddingTop: '20px'
 };
-
-function Sidebar() {
+function Sidebar(props) {
+  const { authLevel } = props;
+  if (!authLevel) {
+    return null; // return null or a placeholder component if authLevel is not defined yet
+  }
+  console.log(`authLevel:sidebar.js `+ authLevel);
   return (
-    <Navbar style={{ backgroundColor: 'black', height: "100%", width: "15%", position: "fixed", zIndex: 1 }}> 
-      {/* Use inline style to change the background to black. Bootstrap variant was removed since it won't work with black color */}
-      <Nav className="flex-column" style={{ height: "100%" }}>
+    <Navbar style={{ backgroundColor: 'black', height: '100%', width: '15%', position: 'fixed', zIndex: 1 }}>
+      <Nav className="flex-column" style={{ height: '100%' }}>
         <Link to="/home" style={linkStyle}>Home</Link>
         <Link to="/bloggs" style={linkStyle}>Bloggs</Link>
         <Link to="/network" style={linkStyle}>Network</Link>
-        <Link to="/admin" style={linkStyle}>Admin</Link>
+        {authLevel === "admin" && <Link to="/admin" style={linkStyle}>Admin</Link>}
       </Nav>
     </Navbar>
   );
 }
-
 export default Sidebar;
